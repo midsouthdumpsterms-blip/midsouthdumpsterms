4003e4f6aad2f73c7d912c4e678e3cf90f0b63a7
Mid South User <user@midsouthdumpsterms.com>
2026-02-12 17:27:56 -0600

    Update Google Analytics tracking ID to G-SQNR36WRXQ

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,23 +2,25 @@
 
 import Script from 'next/script'
 
-export default function GoogleAnalytics({ measurementId }: { measurementId: string }) {
+export default function GoogleAnalytics() {
     return (
         <>
             <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
                 strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-SQNR36WRXQ"
             />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${measurementId}', {
-            page_path: window.location.pathname,
-          });
-        `}
-            </Script>
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-SQNR36WRXQ');
+                    `,
+                }}
+            />
         </>
     )
 }
