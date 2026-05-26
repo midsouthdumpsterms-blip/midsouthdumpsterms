5b574572cb91cda4079c457bca511808c09f2536
Mid South User <user@midsouthdumpsterms.com>
2026-05-25 22:09:25 -0500

    Update review count to 23

--- lib/schema.ts
+++ lib/schema.ts
@@ -114,7 +114,7 @@ export function generateLocalBusinessSchema(): LocalBusinessSchema {
         aggregateRating: {
             '@type': 'AggregateRating',
             ratingValue: '5.0',
-            reviewCount: '20',
+            reviewCount: '23',
             bestRating: '5',
             worstRating: '1',
         },
